97d8c06b3276cb751a0fc54f8ff6fbbb766c65ae
Damon <126731021+damon314159@users.noreply.github.com>
2024-08-03 14:40:42 +0100

    Replace sum method with isEmpty

--- src/modules/Box.js
+++ src/modules/Box.js
@@ -1,10 +1,14 @@
 class Box extends Uint8Array {
   constructor() {
+    // Construct an array of 8 bytes. Each byte is 8 bits
+    // This gives an 8 x 8 grid of bits, representing an 8 x 8 box of alive/dead cells
     super(new ArrayBuffer(8), 0, 8)
   }
 
-  get sum() {
-    return this.reduce((a, b) => a + b, 0)
+  // Check if any cells are alive by summing the bytes. Any alive cell will make the sum non-zero
+  get isEmpty() {
+    const byteSum = this.reduce((a, b) => a + b, 0)
+    return byteSum === 0
   }
 }
 
