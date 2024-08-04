5d493b7d8c140c06439c83877fcd29ed0cb63c91
Damon <126731021+damon314159@users.noreply.github.com>
2024-08-04 18:21:01 +0100

    Memoise BoxRows to further cut render time

--- src/components/BoardContainer/BoxRow/index.jsx
+++ src/components/BoardContainer/BoxRow/index.jsx
@@ -1,13 +1,20 @@
+import { memo } from 'react'
 import Box from '../Box'
 
-function BoxRow({ row, boxRow }) {
-  return (
-    <div className="box-row">
-      {row.map((box, boxCol) => (
-        <Box key={`${boxRow}:${boxCol}`} box={box} />
-      ))}
-    </div>
-  )
-}
+const BoxRow = memo(
+  // eslint-disable-next-line prefer-arrow-callback
+  function BoxRow({ row, boxRow }) {
+    return (
+      <div className="box-row">
+        {row.map((box, boxCol) => (
+          <Box key={`${boxRow}:${boxCol}`} box={box} />
+        ))}
+      </div>
+    )
+  },
+  ({ row: prevRow }, { row: nextRow }) =>
+    prevRow.map((box) => box.join(',')).join(',') ===
+    nextRow.map((box) => box.join(',')).join(',')
+)
 
 export default BoxRow
